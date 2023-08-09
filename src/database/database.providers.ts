9dddc071a8f8e99e5baeca4d3bbeefc90fb90450
arrafii <arrafiialfa.m@gmail.com>
2023-08-09 16:40:04 +0700

    install dotenv and @nestconfig

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,17 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+
 
 export const databaseProviders = [
     {
         provide: 'DATA_SOURCE',
-        useFactory: async () => {
+        useFactory: async (configService: ConfigService) => {
             const dataSource = new DataSource({
                 type: 'mysql',
                 host: 'localhost',
                 port: 3306,
-                username: 'root',
-                password: '281832181',
-                database: 'nestjs_test',
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_DATABASE'),
                 entities: [__dirname + '/../**/*.entity{.ts,.js}'],
                 synchronize: true,
             });
