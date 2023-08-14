82162e270c6aff61309c03585919dec2e74499c8
arrafii <arrafiialfa.m@gmail.com>
2023-08-14 10:50:53 +0700

    add warning log for http status 401,dll

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -5,7 +5,14 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
     private logger = new Logger(`HTTP`);
     use(req: Request, res: Response, next: NextFunction) {
-        this.logger.log(`Logging HTTP request METHOD:${req.method} URL:${req.url} STATUS:${res.statusCode}`);
+        res.on('finish', () => {
+            const statusCode = res.statusCode;
+            if (statusCode === 401 || statusCode === 404 || statusCode === 405 || statusCode === 403 || statusCode === 500) {
+                this.logger.warn(`[${req.method}] ${req.url} - ${statusCode}`);
+            } else {
+                this.logger.log(`Logging HTTP request METHOD:${req.method} URL:${req.url} STATUS:${res.statusCode}`);
+            }
+        });
         next();
     }
 }
