d3e81046a9f08491a4230bb0a644c67e98fbc6f0
arrafii <arrafiialfa.m@gmail.com>
2023-08-14 10:45:53 +0700

    use winston-daily-ratate-file

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as express from 'express';
 import * as multer from 'multer';
 import { WinstonModule } from 'nest-winston';
 import { transports, format } from 'winston';
+import 'winston-daily-rotate-file';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,21 +13,34 @@ async function bootstrap() {
     logger: WinstonModule.createLogger({
       //error logs
       transports: [
-        new transports.File({
-          filename: `logs/error.log`,
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-error.log`,
           level: 'error',
           format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d'
         }),
 
         //combined logs
-        new transports.File({
-          filename: `logs/combined.log`,
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-combined.log`,
           format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d',
         }),
 
         // console logs
         new transports.Console({
-
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
         })
       ]
     })
