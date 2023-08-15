f3d038e556135082d0f53707f56e714f290c3942
arrafii <arrafiialfa.m@gmail.com>
2023-08-15 13:08:47 +0700

    implement mocked user service using ts-jest

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signIn.dto';
 import { JwtModule } from '@nestjs/jwt';
 import { NotFoundException, UnauthorizedException } from '@nestjs/common/exceptions';
 import { BcryptModule } from 'src/bcrypt/bcrypt.module';
-import { UsersModule } from 'src/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
 
 describe('AuthService', () => {
   let authService: AuthService;
+  let userService: DeepMocked<UsersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,11 +17,27 @@ describe('AuthService', () => {
         global: true,
         secret: 'test-secret',
         signOptions: { expiresIn: '1200s' },
-      }), UsersModule, ConfigModule],
-      providers: [AuthService],
+      })],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>(),
+        }
+      ],
     }).compile();
 
     authService = module.get<AuthService>(AuthService);
+    userService = module.get(UsersService);
+
+    //mock usersService.findOne to return :
+    const mockedResolvedUser = {
+      id: 23,
+      name: 'tstname',
+      username: 'test3',
+      password: '$2a$10$KkWrvWRrItS7qO8to2uai./zZizI5Wblk2nXWDfERB7Bn2CJxtlpO' //this is 123123
+    }
+    userService.findOne.mockResolvedValue(mockedResolvedUser);
   });
 
   describe('login', () => {
@@ -29,12 +46,15 @@ describe('AuthService', () => {
         username: 'test3',
         password: '123123'
       };
+
       await expect(authService.signIn(newUser.username, newUser.password)).resolves.toHaveProperty('access_token')
     });
 
     it('wrong username should return notfound exception', async () => {
+      //mock when userService does not return a user object
+      userService.findOne.mockResolvedValue(null);
       const newUser: SignInDto = {
-        username: 'test100000000',
+        username: '',
         password: '123123'
       }
       await expect(authService.signIn(newUser.username, newUser.password)).rejects.toBeInstanceOf(NotFoundException)
