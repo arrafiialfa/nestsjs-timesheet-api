d5977bef109fb49bf3cf055e42b6e4d2ea5b0c32
arrafii <arrafiialfa.m@gmail.com>
2023-08-25 15:48:52 +0700

    add ratelimitermodule

--- src/rate-limiter/rate-limiter.module.ts
+++ src/rate-limiter/rate-limiter.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { RateLimiterService } from './rate-limiter.service';
 
 @Module({
-  providers: [RateLimiterService]
+  providers: [RateLimiterService],
 })
 export class RateLimiterModule { }
--- src/rate-limiter/rate-limiter.service.ts
+++ src/rate-limiter/rate-limiter.service.ts
@@ -1,12 +1,25 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { RateLimiterPostgres } from 'rate-limiter-flexible';
+import { Injectable } from '@nestjs/common';
+import { RateLimiterPostgres } from "rate-limiter-flexible"
+
 
 @Injectable()
 export class RateLimiterService {
 
-    constructor(
-        @Inject('USER_REPOSITORY')
-        private lets
-    ) { }
+    private createRateLimiter(opts: any): Promise<RateLimiterPostgres> {
+        return new Promise<RateLimiterPostgres>((resolve, reject) => {
+            // eslint-disable-next-line prefer-const
+            let rateLimiter
+            const ready = (err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(rateLimiter);
+            }
+
+            rateLimiter = new RateLimiterPostgres(opts, ready)
+
+        })
+    }
+
 
 }
