86a08f257d066f8f8dd48a23f3cd6e2ba55eefbb
arrafii <arrafiialfa.m@gmail.com>
2023-09-05 13:27:59 +0700

    refactor

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,26 +1,31 @@
 /* eslint-disable prettier/prettier */
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv'
+dotenv.config();
 
+const datasource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOSTNAME,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
+});
+
+datasource.initialize();
 
 export const databaseProviders = [
     {
         provide: 'DATA_SOURCE',
-        useFactory: async (configService: ConfigService) => {
-            const dataSource = new DataSource({
-                type: 'postgres',
-                host: configService.get<string>('DB_HOSTNAME'),
-                port: configService.get<number>('DB_PORT'),
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: configService.get<string>('DB_NAME'),
-                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-                synchronize: true,
-            });
+        useFactory: async () => {
+            const dataSource = datasource;
 
             await dataSource.initialize();
             return dataSource;
         },
-        inject: [ConfigService]
     },
 ];
+
+export default datasource
