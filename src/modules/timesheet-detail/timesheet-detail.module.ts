dbab963dea6384cd1f15ff26256795298faa64ac
arrafii <arrafiialfa.m@gmail.com>
2023-09-06 14:38:24 +0700

    check scopeofwork and timesheet in db

--- src/modules/timesheet-detail/timesheet-detail.module.ts
+++ src/modules/timesheet-detail/timesheet-detail.module.ts
@@ -3,11 +3,13 @@ import { TimesheetDetailService } from './timesheet-detail.service';
 import { TimesheetDetailController } from './timesheet-detail.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { timesheetDetailProviders } from 'src/entities/providers/timesheet_detail.providers';
+import { TimesheetService } from '../timesheet/timesheet.service';
+import { ScopeOfWorkService } from '../scope-of-work/scope-of-work.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [TimesheetDetailController],
-  providers: [TimesheetDetailService, ...timesheetDetailProviders],
+  providers: [TimesheetDetailService, TimesheetService, ScopeOfWorkService, ...timesheetDetailProviders],
   exports: [TimesheetDetailService]
 })
 export class TimesheetDetailModule { }
--- src/modules/timesheet-detail/timesheet-detail.service.ts
+++ src/modules/timesheet-detail/timesheet-detail.service.ts
@@ -3,15 +3,42 @@ import { CreateTimesheetDetailDto } from './dto/create-timesheet-detail.dto';
 import { UpdateTimesheetDetailDto } from './dto/update-timesheet-detail.dto';
 import { Repository } from 'typeorm';
 import { TimesheetDetail } from 'src/entities/timesheet_detail.entity';
+import { TimesheetService } from '../timesheet/timesheet.service';
+import { ScopeOfWorkService } from '../scope-of-work/scope-of-work.service';
 @Injectable()
 export class TimesheetDetailService {
 
   constructor(
     @Inject('TIMESHEET_DETAIL_REPOSITORY')
-    private timesheetDetailRepository: Repository<TimesheetDetail>
+    private timesheetDetailRepository: Repository<TimesheetDetail>,
+    private timesheetService: TimesheetService,
+    private scopeOfWorkService: ScopeOfWorkService
   ) { }
 
-  create(createTimesheetDetailDto: CreateTimesheetDetailDto) {
+  async create(createTimesheetDetailDto: CreateTimesheetDetailDto) {
+    //check if timesheet_id exists
+    //check if project_id exists
+    //check if scope_of_work_id exists
+    //else throw error
+
+    let errMssg = null;
+    const timesheet = await this.timesheetService.findOne(createTimesheetDetailDto.timesheet_id)
+    const project = 'project exists'
+    const scopeOfWork = await this.scopeOfWorkService.findOne(createTimesheetDetailDto.scope_of_work_id)
+    if (!timesheet) {
+      errMssg += 'Check your timesheet_id, Timesheet is not found in DB. '
+    }
+    if (!project) {
+      errMssg += 'Check your project_id, Project is not found in DB. '
+    }
+    if (!scopeOfWork) {
+      errMssg += 'Check your scope_of_work_id, Scope of Work is not found in DB. '
+    }
+
+    if (errMssg) {
+      throw new Error(errMssg)
+    }
+
     return this.timesheetDetailRepository.create(createTimesheetDetailDto)
   }
 
