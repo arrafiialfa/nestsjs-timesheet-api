e4a1d92589009f8233a61da6a8d8c0966b75b794
arrafii <arrafiialfa.m@gmail.com>
2023-09-07 15:51:37 +0700

    add create project dto

--- src/modules/project/dto/create-project.dto.ts
+++ src/modules/project/dto/create-project.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty } from "class-validator";
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, isDate } from "class-validator";
 
 export class CreateProjectDto {
     @IsNotEmpty()
@@ -6,4 +6,55 @@ export class CreateProjectDto {
 
     @IsNotEmpty()
     name: string;
+
+    @IsNotEmpty()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    value: number;
+
+    @IsOptional()
+    @IsNumber()
+    unit_owner_id: number | null;
+
+    @IsOptional()
+    @IsNumber()
+    upp_id: number | null;
+
+    @IsOptional()
+    @IsNumber()
+    contractor_id: number | null;
+
+    @IsNotEmpty()
+    start_date: Date;
+
+    @IsOptional()
+    cut_off_date: Date | null;
+
+    @IsOptional()
+    effective_date: Date | null;
+
+    @IsNotEmpty()
+    finish_date: Date;
+
+    @IsOptional()
+    @IsNumber()
+    document_id: number | null;
+
+    @IsOptional()
+    level: string | null;
+
+    @IsOptional()
+    @IsNumber()
+    pic_project_id: number | null;
+
+    @IsOptional()
+    @IsNumber()
+    related_role_owner_id: number | null;
+
+    @IsOptional()
+    @IsNumber()
+    related_role_upp_id: number | null;
+
+    @IsOptional()
+    @IsNumber()
+    consultant_id: number | null;
 }
