9f8dd7fe2eff9d313560cab91e1bba4c1e342164
arrafii <arrafiialfa.m@gmail.com>
2023-09-12 10:58:33 +0700

    add holiday entity

---
+++ src/entities/holiday.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, } from 'typeorm';
+
+@Entity('holidays')
+export class Holiday {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    date: number
+
+    @Column({ length: 191 })
+    day: string
+
+    @Column({ length: 191 })
+    month: string
+
+    @Column()
+    year: number
+
+    @Column({ length: 191 })
+    holiday: string
+
+    @Column({ length: 191 })
+    datetime_ms: string
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+
+}
