e0fd7f897478a629d345667a137fd1a379a43086
arrafii <arrafiialfa.m@gmail.com>
2023-09-28 16:34:33 +0700

    remove project controller

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
-import { ProjectController } from './project.controller';
 import { projectProviders } from 'src/entities/providers/project.providers';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [ProjectController],
   providers: [ProjectService, ...projectProviders],
   exports: [ProjectService, ...projectProviders]
 })
